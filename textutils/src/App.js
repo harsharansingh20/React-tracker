022c771193b899d12e11c8d2e018f891be6abd4d
Harsharan singh <harsharansingh20@gmail.com>
2022-02-11 14:11:25 +0530

    textform

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
+    <>
     <Navbar title="set title here" about="set about here" services = "services" />
+    <TextForm/>
+    </>
   );
 }
 
---
+++ textutils/src/components/TextForm.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function TextForm() {
+  return (
+    
+    <>
+
+<div className="mb-3">
+  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+</div>
+
+
+
+
+
+    </>
+
+
+
+  )
+}
