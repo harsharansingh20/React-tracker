e747d2fe56be92afa03f1bf01791521f8594847a
Harsharan singh <harsharansingh20@gmail.com>
2022-02-11 20:21:27 +0530

    implementing props

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -1,45 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
     
     <>
 
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
+<nav className="navbar navbar-expand-lg navbar-dark bg-black">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">{props.title}</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">{props.about}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">{props.services}</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">{props.about}</a>
+        <a className="nav-link" href="#">Pricing</a>
+      </div>
     </div>
   </div>
 </nav>
-
-
-
-
     </>
+
   )
 }
+  
+
+Navbar.propTypes = { 
+  title : PropTypes.string
+                    
+}
+Navbar.defaultProps = {
+title :  'set title here',
+about : 'About'
+}
+
+
+    
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,15 +1,18 @@
 import React from 'react'
+// import PropTypes from 'prop-types'
 
 export default function TextForm(props) {
   return (
     
     <>
+<div className="container my-5">
 
 <div className="mb-3">
   <label for="exampleFormControlTextarea1" className="form-label">{props.title}</label>
   <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
 </div>
 
+</div>
 
 
 
