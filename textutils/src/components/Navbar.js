2f77296c4f4ce607b21574a1bbfdb5290088ea65
Harsharan singh <harsharansingh@Harsharans-MacBook-Pro.local>
2022-02-11 13:29:49 +0530

    setting props

--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     
     <>
 
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
