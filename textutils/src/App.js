b1ef400eb400b42c3497ac6e5e72885d8becf975
Harsharan singh <harsharansingh20@gmail.com>
2022-02-13 01:12:33 +0530

    enabling dark mode

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
     <>
     <Navbar title="Textutils" about="About" services = "services" />
     <TextForm title="Enter Your Text Here To Analyze Below" />
+    <About/>
     </>
   );
 }
