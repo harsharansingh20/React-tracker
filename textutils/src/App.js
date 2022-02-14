e0e1aec95e26ed62d8746caf9314bab227f357d4
Harsharan singh <harsharansingh20@gmail.com>
2022-02-14 23:43:37 +0530

    usestate

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -7,9 +7,9 @@ import About from './components/About';
 function App() {
   return (
     <>
-    <Navbar title="Textutils" about="About" services = "services" />
+    {/* <Navbar title="Textutils" about="About" services = "services" />
     <TextForm title="Enter Your Text Here To Analyze Below" />
-    {/* <About/> */}
+    <About/> */}
     </>
   );
 }
